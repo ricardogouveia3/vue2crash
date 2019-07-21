9b4295024b01c420397243aaa73ae790cae791c8
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2019-07-21 09:38:47 -0300

    Final course commit

---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is the about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat dolorum quod accusantium enim ex modi sequi ipsa! Harum temporibus a saepe tempore. Deserunt atque ipsam nihil voluptatibus tempora, doloribus dignissimos.</p>
+  </div>
+</template>
